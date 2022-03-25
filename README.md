8a538a85376eb0141da8d34c814ebb36a871a621
Matias <matimacazaga7@gmail.com>
2022-03-25 16:23:38 -0300

    Renamed paths classes.

---
+++ README.md
@@ -0,0 +1,3 @@
+# compfin
+
+A library that implements a variety of techniques and methods related to computational finance and financial engineering.
--- compfin/paths_generator.py
+++ compfin/paths_generator.py
@@ -4,42 +4,43 @@ from numbers import Number
 from typing import Optional
 
 
-class Path(ABC):
-
+class RandomPath(ABC):
     def __init__(self, seed: Optional[Number] = None):
 
         self.random_number_generator = np.random.default_rng(seed)
 
     @abstractmethod
     def generate(
-            self,
-            number_of_paths: int,
-            number_of_steps: int,
-            simulation_time: Number,
-            drift: float,
-            volatility: float,
-            initial_value: float):
+        self,
+        number_of_paths: int,
+        number_of_steps: int,
+        simulation_time: Number,
+        drift: float,
+        volatility: float,
+        initial_value: float,
+    ):
 
         pass
 
 
-class ABM(Path):
-
+class ArithmeticBrownianMotion(RandomPath):
     def generate(
-            self,
-            number_of_paths: int,
-            number_of_steps: int,
-            simulation_time: Number,
-            drift: float,
-            volatility: float,
-            initial_value: float):
+        self,
+        number_of_paths: int,
+        number_of_steps: int,
+        simulation_time: Number,
+        drift: float,
+        volatility: float,
+        initial_value: float,
+    ):
 
         Z = self.random_number_generator.normal(
-            0., 1., size=(number_of_paths, number_of_steps))
+            0.0, 1.0, size=(number_of_paths, number_of_steps)
+        )
 
-        paths = np.zeros(shape=(number_of_paths, number_of_steps+1))
+        paths = np.zeros(shape=(number_of_paths, number_of_steps + 1))
 
-        time = np.linspace(0., simulation_time, num=number_of_steps+1)
+        time = np.linspace(0.0, simulation_time, num=number_of_steps + 1)
 
         paths[:, 0] = initial_value
 
@@ -51,19 +52,34 @@ class ABM(Path):
 
                 Z[:, i] = (Z[:, i] - np.mean(Z[:, i])) / np.std(Z[:, i])
 
-            paths[:, i+1] = paths[:, i] + \
-                (drift - 0.5 * volatility**2) * \
-                dt + volatility * Z[:, i] * dt**0.5
+            paths[:, i + 1] = (
+                paths[:, i]
+                + (drift - 0.5 * volatility ** 2) * dt
+                + volatility * Z[:, i] * dt ** 0.5
+            )
 
         return time, paths
 
 
-class GBM(ABM):
+class GeometricBrownianMotion(ArithmeticBrownianMotion):
+    def generate(
+        self,
+        number_of_paths: int,
+        number_of_steps: int,
+        simulation_time: Number,
+        drift: float,
+        volatility: float,
+        initial_value: float,
+    ):
 
-    def generate(self, number_of_paths: int, number_of_steps: int, simulation_time: Number, drift: float, volatility: float, initial_value: float):
-
-        time, abm_paths = super().generate(number_of_paths, number_of_steps, simulation_time,
-                                           drift, volatility, np.log(initial_value))
+        time, abm_paths = super().generate(
+            number_of_paths,
+            number_of_steps,
+            simulation_time,
+            drift,
+            volatility,
+            np.log(initial_value),
+        )
         return time, np.exp(abm_paths)
 
     def generate_from_abm(abm_paths: np.array):
@@ -72,11 +88,10 @@ class GBM(ABM):
 
 
 class MoneySavingsAccount:
-
     @staticmethod
     def get_discount_rate(drift: float, time: Number):
 
-        return np.exp(-drift*time)
+        return np.exp(-drift * time)
 
 
 if __name__ == "__main__":
@@ -130,28 +145,50 @@ if __name__ == "__main__":
 
     volatility = 0.1
 
-    initial_value = 1.
+    initial_value = 1.0
 
-    gbm = GBM(42)
+    gbm = GeometricBrownianMotion(42)
     time, gbm_paths_q = gbm.generate(
-        number_of_paths, number_of_steps, simulation_time, drift_r, volatility, initial_value)
+        number_of_paths,
+        number_of_steps,
+        simulation_time,
+        drift_r,
+        volatility,
+        initial_value,
+    )
 
     _, gbm_paths_p = gbm.generate(
-        number_of_paths, number_of_steps, simulation_time, drift_mu, volatility, initial_value)
+        number_of_paths,
+        number_of_steps,
+        simulation_time,
+        drift_mu,
+        volatility,
+        initial_value,
+    )
 
     discount_rate = MoneySavingsAccount.get_discount_rate(drift_r, time)
 
     fig, axs = plt.subplots(ncols=2)
 
-    axs[0].plot(time, initial_value*np.exp(drift_r * time) *
-                discount_rate, color="red", linestyle="--", label="Process under Q-Measure")
-    axs[0].plot(time, (gbm_paths_q*discount_rate).T)
+    axs[0].plot(
+        time,
+        initial_value * np.exp(drift_r * time) * discount_rate,
+        color="red",
+        linestyle="--",
+        label="Process under Q-Measure",
+    )
+    axs[0].plot(time, (gbm_paths_q * discount_rate).T)
 
     axs[0].legend()
 
-    axs[1].plot(time, initial_value*np.exp(drift_mu*time) *
-                discount_rate, color="red", linestyle="--", label="Process under P-Measure")
-    axs[1].plot(time, (gbm_paths_p*discount_rate).T)
+    axs[1].plot(
+        time,
+        initial_value * np.exp(drift_mu * time) * discount_rate,
+        color="red",
+        linestyle="--",
+        label="Process under P-Measure",
+    )
+    axs[1].plot(time, (gbm_paths_p * discount_rate).T)
 
     axs[1].legend()
     plt.show()
